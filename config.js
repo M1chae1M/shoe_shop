f54fa1eb834889f7fc8e21b573634633f6448718
M1chae1M <michalmaru10@gmail.com>
2023-05-11 12:10:09 +0200

    can build

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 // export const API_READY = process.env.API_READY === 'true';
-// module.exports = {
-//   env: {
-//     API_READY: false,
-//   },
-// };
+module.exports = {
+  env: {
+    API_READY: false,
+  },
+};
--- pages/products/[boot].js
+++ pages/products/[boot].js
@@ -1,14 +1,23 @@
 import React, {Component} from "react";
 import {API_READY} from '../../config';
+import fetch from 'node-fetch';
+import HOC,{Store} from "../HOC";
+import {glbl} from "../_app";
+import ImgFrame from "../ImgFrame";
+import GreenBTN from "../GreenBTN";
+import Number from "../Number";
+import Router from "next/router";
+import SelectC, {OptionC} from "../SelectC";
+import Link from "next/link";
 
 export async function getStaticPaths(){
 
-  // if(!API_READY){
-  //   return {
-  //     paths: [],
-  //     fallback: 'blocking',
-  //   };
-  // }
+  if(!API_READY){
+    return {
+      paths: [],
+      fallback: 'blocking',
+    };
+  }
 
   const paths=await fetch(`${process.env.NEXT_PUBLIC_API_URL}products`)
   .then(res=>res.json())
@@ -19,162 +28,115 @@ export async function getStaticPaths(){
   })
   return{
     paths,
-    // fallback:false,
-    fallback: 'blocking',
+    fallback:false,
   };
 }
 
-export async function getStaticProps(context){
-  const {boot}=context.params;
-  return{props:{boot}}
+export async function getStaticProps(ctx){
+  const {boot}=ctx.params;
+
+  const res=await fetch(`${process.env.NEXT_PUBLIC_API_URL}singleProduct`,{
+    method:'POST',
+    headers:{'Content-Type':'application/json'},
+    body:JSON.stringify({id:boot})
+  })
+  .then(resp=>resp.json())
+  .then(resp=>resp.resp);
+  const data=res;
+  return{props:{data}}
 }
 
-// export async function getStaticPaths(){
-//   const products=await fetch(`${process.env.NEXT_PUBLIC_API_URL}products`);
-//   const data=await products.json();
-//   const paths=data.resp.map((product)=>({
-//     params:{boot:product.id.toString()},
-//   }));
-//   return{
-//     paths,
-//     fallback:false,
-//   };
-// }
-
-// export async function getStaticProps(ctx){
-//   const res=await fetch(`${process.env.NEXT_PUBLIC_API_URL}singleProduct`,{
-//       method:'POST',
-//     headers:{'Content-Type':'application/json'},
-//     body:JSON.stringify({id:ctx.params.boot})
-//   })
-//   .then(resp=>resp.json())
-//   .then(resp=>resp.resp);
-//   const data=res;
-//   return{props:{data}}
-// }
-
-export default class Boot extends Component{
+class Boot extends Component{
   state={
-    data:{},
-    // sizeState:this.state?.data?.sizes.split(',')[0]||'0',
+    sizeState:this.props?.data?.sizes.split(',')[0]||'0',
     howMany:1,
   }
-  // componentDidMount(){
-    // fetch(`${process.env.NEXT_PUBLIC_API_URL}singleProduct`,{
-    // fetch(`/api/singleProduct`,{
-    //   method:'POST',
-    //   headers:{'Content-Type':'application/json'},
-    //   body:JSON.stringify({id:this.props.boot})
-    // })
-    // .then(resp=>resp.json())
-    // .then(resp=>resp.resp)
-    // .then(resp=>this.setState({data:resp}))
-  // }
   render(){
-    // const {data}=this.props;
-    // const {id,image,sex,description,category,name,price,quantity,sizes}=data;
-    // const {sizeState,howMany}=this.state;
-    // const styles={
-    //   Product:{
-    //     width:'70vw',
-    //     padding:'5px',
-    //     display:'grid',
-    //     justifyItems:'center',
-    //     alignItems:'center',
-    //   },
-    //   btn:{
-    //     width:'fit-content',
-    //     padding:'5px',
-    //   },
-    //   description:{
-    //     width:glbl._width,
-    //     padding:'5px',
-    //   },
-    //   path:{
-    //     padding:'5px',
-    //   },
-    //   Link:{
-    //     color:'black',
-    //     textDecoration:'none',
-    //   },
-    //   change:{
-    //     display:'grid',
-    //     gridAutoFlow:'column',
-    //     justifyItems:'center',
-    //     alignItems:'center',
-    //   },
-    // }
-
-
-    // const changeHowMany=(change)=>{
-    //   const value=howMany+change;
-    //   if(value<=quantity && value>0){
-    //     this.setState({howMany:value});
-    //   }
-    // }
-    // const addToCartNewBoot=()=>{
-    //   Router.push('/');
-    //   return {boot,name,image,price,howMany,sizeState,time:new Date().getTime()}
-    // }
-    // const onChange=(e)=>this.setState({sizeState:e.target.value})
-    // const links=[
-    //   {href:'/',text:'Boots > '},
-    //   {href:`/?sex=${sex}`,text:`${sex} > `},
-    //   {href:`/?type=${category}`,text:`${category} > `},
-    //   {href:`/products/${boot}`,text:name},
-    // ]
+    const {data}=this.props;
+    const {id,image,sex,description,category,name,price,quantity,sizes}=data;
+    const {sizeState,howMany}=this.state;
+    const styles={
+      Product:{
+        width:'70vw',
+        padding:'5px',
+        display:'grid',
+        justifyItems:'center',
+        alignItems:'center',
+      },
+      btn:{
+        width:'fit-content',
+        padding:'5px',
+      },
+      description:{
+        width:glbl._width,
+        padding:'5px',
+      },
+      path:{
+        padding:'5px',
+      },
+      Link:{
+        color:'black',
+        textDecoration:'none',
+      },
+      change:{
+        display:'grid',
+        gridAutoFlow:'column',
+        justifyItems:'center',
+        alignItems:'center',
+      },
+    }
+    const changeHowMany=(change)=>{
+      const value=howMany+change;
+      if(value<=quantity && value>0){
+        this.setState({howMany:value});
+      }
+    }
+    const addToCartNewBoot=()=>{
+      Router.push('/');
+      return {id,name,image,price,howMany,sizeState,time:new Date().getTime()}
+    }
+    const onChange=(e)=>this.setState({sizeState:e.target.value})
+    const links=[
+      {href:'/',text:'Boots > '},
+      {href:`/?sex=${sex}`,text:`${sex} > `},
+      {href:`/?type=${category}`,text:`${category} > `},
+      {href:`/products/${id}`,text:name},
+    ]
     return(
-      <div>
-        Boot
-        {console.log(this.props)}
-
-
-      </div>
+      <Store.Consumer>
+      {value=>{
+        const {saveToLocalStorage}=value??{};
+        const addToCart=(e)=>{
+          saveToLocalStorage(e,addToCartNewBoot())
+        }
+        return(
+          <div style={styles.Product}>
+            <div style={styles.path}>
+              {links.map(({href,text})=><Link key={text} style={styles.Link} href={href}>{text}</Link>)}
+            </div>
+            <ImgFrame src={image} alt='' x={glbl._img_height} y={glbl._img_height}/>
+            <div style={styles.description}>{description}</div>
+            <div style={styles.path}>Price: {price} PLN</div>
+            <div style={styles.path}>
+              {'Sizes: '}
+              <SelectC value={sizeState} onChange={onChange} className="GreenBTN">
+                {sizes.split(',').map(x=><OptionC key={x} value={x}>{x}</OptionC>)}
+              </SelectC>
+            </div>
+            <div style={styles.path}>Available quantity: {quantity}</div>
+            <div style={{...styles.path,...styles.change}}>
+              how many boots you need?
+              <Number value={howMany} onChange={changeHowMany}/>
+            </div>
+            <div style={styles.btn}>
+              <GreenBTN value='Add to cart' onClick={addToCart} className="GreenBTN"/>
+            </div>
+          </div>
+        )
+      }}
+      </Store.Consumer>
     )
   }
 }
 
-
-
-
-//<Store.Consumer>
-//  {value=>{
-//    // const {saveToLocalStorage}=value??{};
-//    // const addToCart=(e)=>{
-//    //   saveToLocalStorage(e,addToCartNewBoot())
-//    // }
-//    return(
-//      <div style={styles.Product}>
-//        {/* <div style={styles.path}>
-//          {links.map(({href,text})=><Link key={text} style={styles.Link} href={href}>{text}</Link>)}
-//        </div>
-//        <ImgFrame src={image} alt='' x={glbl._img_height} y={glbl._img_height}/>
-//        <div style={styles.description}>{description}</div>
-//        <div style={styles.path}>Price: {price} PLN</div>
-//        <div style={styles.path}>
-//          {'Sizes: '}
-//          <SelectC value={sizeState} onChange={onChange} className="GreenBTN">
-//            {sizes.split(',').map(x=><OptionC key={x} value={x}>{x}</OptionC>)}
-//          </SelectC>
-//        </div>
-//        <div style={styles.path}>Available quantity: {quantity}</div>
-//        <div style={{...styles.path,...styles.change}}>
-//          how many boots you need?
-//          <Number value={howMany} onChange={changeHowMany}/>
-//        </div>
-//        <div style={styles.btn}>
-//          <GreenBTN value='Add to cart' onClick={addToCart} className="GreenBTN"/>
-//        </div> */}
-//      </div>
-//    )
-//  }}
-//  </Store.Consumer>
-
-
-//     return(
-      
-//     )
-//   }
-// }
-
-// export default HOC(Boot);
+export default HOC(Boot);
