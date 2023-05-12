177e847cf210bd89c359acd17bfd3975340c1220
M1chae1M <michalmaru10@gmail.com>
2023-05-12 13:12:59 +0200

    new folder structure

--- pages/api/order/[order].js
+++ pages/api/order/[order].js
@@ -1,27 +1,7 @@
-// import {connection} from './connectionWithDB';
-// import auth from './authPromise';
-
-// module.exports=(req, res)=>{
-//   const {body}=req;
-//   const {id}=body;
-//   auth(req, res)
-//   .then(({data,login})=>{
-//     if(data?.length>0){
-//       const query=`SELECT * FROM orders WHERE user="${login}" AND id='${id}'`;
-//       connection.query(query, (err,data)=>{
-//         if(data.length>0) res.status(200).json({data:data[0],message:''});
-//         else res.status(200).json({data:[],message:'no orders'})
-//       })
-//     }
-//     else res.status(200).json({data:[],message:'no orders'})
-//   })
-// }
-
 import {connection} from '../connectionWithDB';
 import auth from '../authPromise';
 
 module.exports=(req, res)=>{
-  const {body}=req;
   const {order}=req.query;
   auth(req, res)
   .then(({data,login})=>{
